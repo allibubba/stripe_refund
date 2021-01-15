56e6701ae3daaff24b4cf147a8b76398e2104cd5
Jackson Oates <jackson.oates@gmail.com>
2021-01-15 13:36:57 -0500

    write to relative location

--- tests/lib/ReportWriter.test.js
+++ tests/lib/ReportWriter.test.js
@@ -4,26 +4,39 @@
 
 import chai, {expect} from 'chai';
 import sinon from 'sinon';
-import ReportWriter from '../../lib/ReportWriter';
+import reportWriter from '../../lib/ReportWriter';
 
 const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
+
+const validMessage = {
+  "id": "re_1I8qW82eZvKYlo2C3TOlqHdT",
+  "object": "refund",
+  "amount": 100,
+  "balance_transaction": null,
+  "charge": "ch_1I8qW82eZvKYlo2C3gDngLtS",
+  "created": 1610471932,
+  "currency": "usd",
+  "metadata": {'order_id': '276109', 'foo':'bar'},
+  "payment_intent": null,
+  "reason": null,
+  "receipt_number": null,
+  "source_transfer_reversal": null,
+  "status": "succeeded",
+  "transfer_reversal": null
+}
 
 describe('ReportWriter', () => {
   let reportWriterInstance;
 
   context('success', () => {
-    it('initializes', async () => {
-      reportWriterInstance = new ReportWriter("test");
-      expect(reportWriterInstance.message).to.equal("test");
-    });
-
     it('writes', () => {
-      let message = "never gonna give you up"
-      reportWriterInstance = new ReportWriter(message)
-      reportWriterInstance.write('0001');
-      reportWriterInstance.end()
-      expect(reportWriterInstance.message).to.equal(message);
+      reportWriter.write(validMessage);
+      reportWriter.end();
+      // console.log('∆ -------------------------------------')
+      // console.log(reportWriter)
+      console.log('∆ -------------------------------------')
+      expect(true).to.equal(false);
     });
   });
 });
